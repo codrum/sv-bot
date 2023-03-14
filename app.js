e52141235543001f96654500c340c193d0cd3cd2
codrum <landrum287@hotmail.com>
2023-03-13 19:18:47 -0500

    rewrite and slow attack speed

--- app.js
+++ app.js
@@ -20,19 +20,39 @@ const createBot = () => {
 
     })
     bot1.on('spawn', function () {
-        bot1.loadPlugin(require('mineflayer-autoclicker'))
-        bot1.autoclicker.options = {
-            max_distance: 3.5, // Max distance to hit entities (Default: 3.5)
-            swing_through: ['experience_orb'], // Hit through entities (Default: ['experience_orb'])
-            blacklist: ['player'], // Do not hit certain entities (Default: ['player'])
-            stop_on_window: true, // Stop if a window is opened (Default: true)
-            always_swing: true, // Always swing, even if there is no entity (Default: true)
-            delay: 1500
-        }
-        bot1.autoclicker.start();
+        // bot1.loadPlugin(require('mineflayer-autoclicker'))
+        // bot1.autoclicker.options = {
+        //     max_distance: 3.5, // Max distance to hit entities (Default: 3.5)
+        //     swing_through: ['experience_orb'], // Hit through entities (Default: ['experience_orb'])
+        //     blacklist: ['player'], // Do not hit certain entities (Default: ['player'])
+        //     stop_on_window: true, // Stop if a window is opened (Default: true)
+        //     always_swing: false, // Always swing, even if there is no entity (Default: true)
+        //     // delay: getRandomDelay()
+        // }
+        if (autoClicker.running) {
+            autoClicker.stop();
+            autoClicker.start();
+        } else { autoClicker.start(); }
     });
+    const autoClicker = {
+        running: undefined,
+        click_interval: 3000,
+        // blacklist: ['experience_orb'],
+        start: () => {
+            if (autoClicker.running) return
+            autoClicker.running = setInterval(async function () {
+                const entity = bot1.entityAtCursor()
+                if (!entity || autoClicker.blacklist.includes(entity.name)) return bot1.swingArm()
+                bot1.attack(entity, true)
+            }, autoClicker.click_interval)
+        },
+        stop: () => {
+            autoClicker.running = clearInterval(autoClicker.running)
+        }
+    }
 
     bot1.on('error', (err) => console.log(err))
+    bot1.on('kicked', console.log)
     bot1.on('end', restart)
     bot2.on('error', (err) => console.log(err))
     bot2.on('end', restart)
@@ -42,4 +62,12 @@ const restart = async () => {
     await delay(10000)
     createBot()
 }
+
+const getRandomDelay = () => {
+    const random = Math.random() * (5000 - 700) + 700
+    console.log(random)
+    return random
+}
+
+
 createBot()
